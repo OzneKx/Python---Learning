d9cfeee1fca91ab3c66673798c12d870289da379
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-08 12:09:38 -0300

    Delete Exercise_PositiveNumberOpposite.py

--- Exercise_PositiveNumberOpposite.py
+++
@@ -1,4 +0,0 @@
-numeroPositivoInvertido = input('Digite um número inteiro positivo.: ')
-print('Agora vou mostra-lo invertido: {}'.format(numeroPositivoInvertido[:: -1]))
-numeroPositivoInvertido = input('Digite um número inteiro positivo.: ')
-print('Agora vou mostra-lo invertido: {}'.format(numeroPositivoInvertido[:: -1]))
