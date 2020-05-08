2b43a73bd03bf671bb37f4e50b741504a1a3b33f
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-08 12:07:45 -0300

    Ex 46 - Positive Number Opposite

---
+++ Exercise_PositiveNumberOpposite.py
@@ -0,0 +1,3 @@
+numeroPositivoInvertido = input('Digite um número inteiro positivo.: ')
+print('Agora vou mostra-lo invertido: {}'.format(numeroPositivoInvertido[:: -1]))numeroPositivoInvertido = input('Digite um número inteiro positivo.: ')
+print('Agora vou mostra-lo invertido: {}'.format(numeroPositivoInvertido[:: -1]))
