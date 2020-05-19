1e2e420a9c845cf804b3527d54ed488c4d422868
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-19 20:38:22 -0300

    Exercise - Client Register

--- Projetos/Python - Maicris/Exercise_ClientRegister.py
+++ Projetos/Python - Maicris/Exercise_ClientRegister.py
@@ -4,7 +4,7 @@ Uso de Dicionários como banco de dados
 Estrutura:
 
 clients {
-    id: {
+    id : {
         name: ,
         city: ,
         age:
@@ -17,32 +17,47 @@ CONST_FIELD_CITY = "city"
 CONST_FIELD_AGE = "age"
 
 
-# Limpa o banco de dados
+# Validando dados inteiros em um determinado range
+def get_int_value_with_range(message, min_value, max_value):
+    while True:
+        try:
+            op = int(input(message + ": "))
+        except ValueError:
+            print("Formato inválido: esperado um número")
+            continue
+        if not min_value <= op <= max_value:
+            print("Opção inválida: escolha um número de",
+                  min_value, "a", max_value)
+        else:
+            return op
+
+            
+# Apaga o banco de dados
 def database_clear():
     pass
 
 
-# Pesquisa Clientes
+# Pesquisa clientes
 def client_query():
     pass
 
 
-# Deleta clientes
+# Exclui clientes
 def client_del():
     pass
 
 
-# Edita os dados dos clientes
+# Edita os dados de clientes
 def client_edit():
     pass
 
 
-# Edita os dados de clientes
-def client_register(clients):
-    id = input('Informe o CPF do cliente: ')
-    name = input('Informe o nome do cliente: ')
-    city = input('Informe a cidade do cliente: ')
-    age = get_int_value_with_range(f'Digite a idade do cliente {18} {120}')
+# Cadastra clientes
+def client_add(clients):
+    id = input("Por favor, digite o CPF do cliente: ")
+    name = input("Por favor, digite o nome do cliente: ")
+    city = input("Por favor, digite a cidade do cliente: ")
+    age = get_int_value_with_range("Digite a idade do cliente", 18, 120)
     if id in clients:
         return False
     else:
@@ -54,54 +69,38 @@ def client_register(clients):
         return True
 
 
-# Validando dados inteiros em um determinado range
-def get_int_value_with_range(message, min_value, max_value):
-    while True:
-        try:
-            escolha = int(input(f'{message}: '))
-        except ValueError:
-            print('Formato inválido: esperado número')
-            continue
-        if not min_value <= escolha <= max_value:
-            print(
-                f'Opção inválida: escolha um número de {min_value} a {max_value}')
-        else:
-            return escolha
-
-            
 # Exibe o menu de opções
 def menu():
-    print('Sistema de Cadastro de Clientes')
-    print('[1] Cadastrar Cliente\n'
-          '[2] Alterar dados de cliente\n'
-          '[3] Excluir clientez\n'
-          '[4] Pesquisar Cliente\n'
-          '[5] Zerar Banco de Dados\n'
-          '[6] SAIR')
-    return get_int_value_with_range(f'Digite uma das opções: {1} a {6}')
+    print("*** Sistema de Cadastro de Clientes ***")
+    print("1. Cadastrar cliente")
+    print("2. Alterar dados de cliente")
+    print("3. Excluir cliente")
+    print("4. Pesquisar cliente")
+    print("5. Zerar banco de dados")
+    print("6. Sair")
+    print("***************************************\n")
+    return get_int_value_with_range("Digite uma das opções", 1, 6)
 
 
 # Ponto de entrada do módulo
 def main():
-    clients = {}  # Banco de dados no formato dicionário
+    clients = {}  # banco de dados no formato dicionário
     while True:
-        escolha = menu()
-        if escolha == 1:
-            if client_register(clients):
+        op = menu()
+        if op == 1:
+            if client_add(clients):
                 "Cliente cadastrado com sucesso"
             else:
-                "Problemas ao cadastrar o cliente"
-        elif escolha == 2:
-
-        elif escolha == 3:
+                "Problemas no cadastro do cliente"
+        elif op == 2:
             pass
-        elif escolha == 3:
+        elif op == 3:
             pass
-        elif escolha == 4:
+        elif op == 4:
             pass
-        elif escolha == 5:
+        elif op == 5:
             pass
-        elif escolha == 6:
+        elif op == 6:
             break
 
 
