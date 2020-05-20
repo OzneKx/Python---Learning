2fef524762e5f74e16f8a7f69b68429f766e8d14
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-20 07:48:58 -0300

    Exercise - Client Register

--- Projetos/Python - Maicris/Exercise_ClientRegister.py
+++ Projetos/Python - Maicris/Exercise_ClientRegister.py
@@ -12,12 +12,27 @@ clients {
 }
 """
 
+
+#Keys do dicionário
 CONST_FIELD_NAME = "name"
 CONST_FIELD_CITY = "city"
 CONST_FIELD_AGE = "age"
 
 
-# Validando dados inteiros em um determinado range
+#Exibe o menu de opções
+def menu():
+    print('=-= Sistema de Cadastro de Clientes =-=')
+    print('[1] Cadastrar Cliente\n'
+          '[2] Alterar dados de cliente\n'
+          '[3] Excluir cliente\n'
+          '[4] Pesquisar Cliente\n'
+          '[5] Zerar Banco de Dados\n'
+          '[6] SAIR')
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+    return get_int_value_with_range("Digite uma das opções", 1, 6)
+
+
+#Validando dados inteiros em um determinado range
 def get_int_value_with_range(message, min_value, max_value):
     while True:
         try:
@@ -30,28 +45,8 @@ def get_int_value_with_range(message, min_value, max_value):
         else:
             return escolha
 
-            
-# Apaga o banco de dados
-def database_clear():
-    pass
 
-
-# Pesquisa clientes
-def client_query():
-    pass
-
-
-# Exclui clientes
-def client_del():
-    pass
-
-
-# Edita os dados de clientes
-def client_edit():
-    pass
-
-
-# Cadastra clientes
+#Cadastra clientes
 def client_register(clients):
     id = input('Informe o CPF do cliente: ')
     name = input('Informe o nome do cliente: ')
@@ -65,42 +60,84 @@ def client_register(clients):
             CONST_FIELD_CITY: city,
             CONST_FIELD_AGE: age
         }
+        print('Cliente cadastrado com sucesso! ')
         return True
 
 
-# Exibe o menu de opções
-def menu():
-    print("=-= Sistema de Cadastro de Clientes =-=")
-    print('[1] Cadastrar Cliente\n'
-          '[2] Alterar dados de cliente\n'
-          '[3] Excluir cliente\n'
-          '[4] Pesquisar Cliente\n'
-          '[5] Zerar Banco de Dados\n'
-          '[6] SAIR')
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    return get_int_value_with_range("Digite uma das opções", 1, 6)
+#Edita os dados de clientes
+def client_edit(clients):
+    editar = int(input('[1] Nome\n'
+                       '[2] Cidade\n'
+                       '[3] Idade\n'
+                       'Faça sua escolha: '))
+    if editar == 1: 
+        clients['name'] = input('Nome substituto: ')
+    elif editar == 2:
+        clients['city'] = input('Cidade substituta: ')
+    else:
+        clients['age'] = input('Idade substituta: ')
 
 
-# Ponto de entrada do módulo
+#Remove clientes
+def client_del(clients):
+    cpf = input('Informe o cpf do cliente que deseja remover: ')        #Remoção do cliente através do CPF
+    if cpf == id:
+        for v in clients.values():
+            clients.pop(v)
+
+
+#Pesquisa clientes
+def client_query(clients):
+    buscar = input('Informe o cpf do cliente que deseja pesquisar: ')   #Pesquisa do cliente através do CPF
+    if buscar in clients:
+        clients[buscar]
+    print(clients[buscar])
+
+#Exclui o banco de dados
+def database_clear(clients):
+    really = int(input('Certeza de que gostaria de deletar o banco de dados?\n' #Confirmar se deseja mesmo deletar o banco de dados
+                       '[1] SIM\n'
+                       '[2] NÃO\n'
+                       'Escolha: '))
+    if really == 1:
+        clients.clear()
+    else:
+        print('O banco de dados não foi deletado! ')
+
+
+#Ponto de entrada do módulo
 def main():
-    clients = {}  # banco de dados no formato dicionário
+    clients = {}                                    #Banco de dados no formato dicionário
     while True:
         escolha = menu()
         if escolha == 1:
             if client_register(clients):
-                "Cliente cadastrado com sucesso"
+                'Cliente cadastrado com sucesso! '
             else:
-                "Problemas no cadastro do cliente"
+                'Problemas no cadastro do cliente! '
         elif escolha == 2:
-            pass
-        elif escolha == 3: pass
-        elif escolha == 3: pass
-        elif escolha == 4: pass
-        elif escolha == 5: pass
+            if client_edit(clients):
+                'Dados alterados com sucesso!'
+            else:
+                'Problemas na alteração de dados do cliente! '
+        elif escolha == 3:
+            if client_del(clients):
+                'Cliente exluido com sucesso'
+            else:
+                'Problemas na exclusão do cliente! '
+        elif escolha == 4:
+            if client_query(clients):
+                print('Cliente encontrado com sucesso! ')
+        elif escolha == 5:
+            if database_clear(clients):
+                'Banco de dados limpo com sucesso! '
+            else:
+                'Problemas ao deletar o banco de dados! '
         elif escolha == 6: 
             print('Volte sempre! ')
             break
 
 
+#Modularização
 if __name__ == '__main__':
     main()
