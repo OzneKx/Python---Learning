e9ff4515d70cfae03470610d18eb028e36f337be
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-20 07:54:13 -0300

    SOMATIVA •Em equipes de 3 integrantes •Produza um vídeo de no máximo 3 min de um sistema de uma empresa real, mostrando as entradas, processamento e saída. •Apresentar na próxima aula. •Postar no Blackboard. Observação: Pode ser a mesma empresa que você escolheu para o projeto da disciplina, ou qualquer outra empresa que você tenha acesso.

--- Projetos/Python - Maicris/Exercise_ClientRegister.py
+++ Projetos/Python - Maicris/Exercise_ClientRegister.py
@@ -96,7 +96,7 @@ def client_query(clients):
 
 #Exclui o banco de dados
 def database_clear(clients):
-    really = int(input('Certeza de que gostaria de deletar o banco de dados?\n'  #Confirmar se deseja mesmo deletar o banco de dados
+    really = int(input('Certeza de que gostaria de deletar o banco de dados?\n'   #Confirmar se deseja mesmo deletar o banco de dados
                        '[1] SIM\n'
                        '[2] NÃO\n'
                        'Escolha: '))
@@ -135,7 +135,7 @@ def main():
             else:
                 'Problemas ao deletar o banco de dados! '
         elif escolha == 6: 
-            print('Volte sempre! ')                             #Saída do programa com mensagem de agradecimento ao usuário
+            print('Volte sempre! ')                              #Saída do programa com mensagem de agradecimento ao usuário
             break
 
 
