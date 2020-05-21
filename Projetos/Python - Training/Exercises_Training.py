366f98c21bee4654e99892d1812cc4b10affd7eb
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-21 07:51:10 -0300

    Exercises - Training

--- Projetos/Python - Training/Exercises_Training.py
+++ Projetos/Python - Training/Exercises_Training.py
@@ -1,4 +1,4 @@
-dict_alunos = {}
+'''dict_alunos = {}
 dict_alunos["Nome"] = str(input('Nome do aluno: '))
 media = float(input('Média do aluno: '))
 dict_alunos["Média"] = media
@@ -9,4 +9,17 @@ elif 5 < media < 7:
 elif media >= 7:
   dict_alunos["Situação"] = 'Aprovado'
 for k, v in dict_alunos.items():
-  print(f'{k} = {v}')
+  print(f'{k} = {v}')'''
+
+
+from operator import itemgetter
+dict_dice = {}
+from random import randint
+for c in range(1, 4 + 1, 1):
+  dict_dice[c] = randint(1, 7)
+  print(f'O jogador {c} tirou {dict_dice[c]}')
+print('RANKING FINAL')
+maior = 0
+for k, v in sorted(dict_dice.items(), key=itemgetter(1), reverse=True):
+  maior += 1
+  print(f'{k} ficou em {maior}° lugar, com: {v} pontos. ')
