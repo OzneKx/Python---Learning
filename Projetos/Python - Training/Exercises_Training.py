ec0ae984737a52bf5830ffa09d35e0d530e09e9d
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-20 22:31:39 -0300

    Exercises - Training

---
+++ Projetos/Python - Training/Exercises_Training.py
@@ -0,0 +1,12 @@
+dict_alunos = {}
+dict_alunos["Nome"] = str(input('Nome do aluno: '))
+media = float(input('Média do aluno: '))
+dict_alunos["Média"] = media
+if media <= 5:
+  dict_alunos["Situação"] = 'Reprovado'
+elif 5 < media < 7:
+  dict_alunos["Situação"] = 'Recuperação'
+elif media >= 7:
+  dict_alunos["Situação"] = 'Aprovado'
+for k, v in dict_alunos.items():
+  print(f'{k} = {v}')
