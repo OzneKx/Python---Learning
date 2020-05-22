00da13abfa79d0c7a41290b26766320bdfbc7a0e
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-05-21 22:59:45 -0300

    Exercises - Training

--- Projetos/Python - Training/Exercises_Training.py
+++ Projetos/Python - Training/Exercises_Training.py
@@ -12,7 +12,7 @@ for k, v in dict_alunos.items():
   print(f'{k} = {v}')'''
 
 
-from operator import itemgetter
+'''from operator import itemgetter
 dict_dice = {}
 from random import randint
 for c in range(1, 4 + 1, 1):
@@ -22,4 +22,23 @@ print('---------RANKING FINAL---------')
 maior = 0
 for k, v in sorted(dict_dice.items(), key=itemgetter(1), reverse=True):
   maior += 1
-  print(f'{k} ficou em {maior}° lugar, com: {v} pontos. ')
+  print(f'{k} ficou em {maior}° lugar, com: {v} pontos. ')'''
+
+
+from datetime import date
+ano_atual = date.today().year
+dict_cadastro = {"Nome": 0 ,
+                 "Carteira de Trabalho": 0, 
+                 "Ano de Contratação": 0, "Salário": 0
+                 }
+for k in dict_cadastro.keys():
+  dict_cadastro[k] = input(f'Digite seu {k}: ')
+  nascimento = int(input('Ano de nascimento: '))
+  dict_cadastro["Idade"] = (ano_atual - nascimento)
+  dict_cadastro["Anos restantes de Trabalho"] = ((ano_atual - dict_cadastro["Ano de Contratação"]) +  dict_cadastro["Idade"])
+  if dict_cadastro["Anos restantes de Trabalho"] > 35:
+    print(f'Você já está aposentado! ')
+  else:
+    print(f'Ainda faltam {((ano_atual - dict_cadastro["Ano de Contratação"]) +  dict_cadastro["Idade"])} anos para se aposentar. ')
+for k, v in dict_cadastro.items():
+  print(f'{k} tem valor igual a = {v}')
