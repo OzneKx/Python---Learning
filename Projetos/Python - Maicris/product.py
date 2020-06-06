b8e8980433efe79ee253e264d64ab0d051e3cea1
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-06-06 11:20:58 -0300

    Products

--- Projetos/Python - Maicris/product.py
+++ Projetos/Python - Maicris/product.py
@@ -4,7 +4,6 @@ Guitar Registration
 
 
 import utilities as util
-import pickle
 
 
 CONST_FIELD_MODEL = "Modelo"
@@ -13,57 +12,83 @@ CONST_FIELD_YEAR = "Ano de Fabricação"
 CONST_FIELD_COUNTRY = "Origem de Fabricação"
 CONST_FIELD_COST = "Custo"
 CONST_FIELD_DESCRIPTION = "Descrição"
+CONST_FIELD_TOTAL = "Itens no Estoque"
 
 
 def menu():
-    util.ornament('Cadastro de Guitarras')
-    util.ornament_two('-----SUAS OPÇÕES-----')
+    """
+    Exibe menu de opções
+
+    :return: Opção escolhida pelo usuário
+    """
+    print('Cadastro de Guitarras')
+    print('-----SUAS OPÇÕES-----')
     print('[1] Cadastrar Guitarras\n'
           '[2] Alterar Guitarras\n'
           '[3] Pesquisar Guitarras\n'
           '[4] Listar Guitarras\n'
-          '[5] Remover Estoque\n'
+          '[5] Alterar Estoque\n'
           '[6] SAIR')
     print('=' * 23)
     return util.get_int_value_with_range('Digite uma das opções: ', 1, 6)
 
 
-def guitar_register(guitars: dict):
+def guitar_register(serial: int, guitars: dict):
     """
     Registrar guitarra e suas especificações
 
-    :param guitars: Características da guitarra ao cadastrá-la
+    :param serial: Registro do número de série no dicionário
+    :param guitars: Caracteristicas da guitarra ao cadastra-la
     :return: None
     """
-    serial = util.get_int_value('Informe o número de série da guitarra: ')
-    model = str(input('Modelo da guitarra: \n'
-                      ' -> SG\n'
-                      ' -> Les Paul\n'
-                      ' -> Explorer\n'
-                      ' -> Flying V\n'
-                      ' -> Telecaster\n'
-                      ' -> Stratocaster\n'
-                      'Escolha: ')).capitalize().strip()
-    brand = input('Fabricante da guitarra: \n'
-                  ' -> Gibson\n'
-                  ' -> Fender\n'
-                  ' -> Ibanez\n'
-                  ' -> Yamaha\n'
-                  ' -> Epiphone\n'
-                  'Escolha (Se for outra, digite aqui): ').capitalize().strip()
+    model = util.get_str_value('Modelo da guitarra: \n'
+                               ' -> SG\n'
+                               ' -> Les Paul\n'
+                               ' -> Explorer\n'
+                               ' -> Flying V\n'
+                               ' -> Telecaster\n'
+                               ' -> Stratocaster\n'
+                               'Escolha: ')
+    brand = util.get_str_value('Fabricante da guitarra: \n'
+                               ' -> Gibson\n'
+                               ' -> Fender\n'
+                               ' -> Ibanez\n'
+                               ' -> Yamaha\n'
+                               ' -> Epiphone\n'
+                               'Escolha (Se for outra, digite aqui): ')
     year = util.get_int_value_with_range('Ano de fabricação: ', 1931, util.actual_time())
-    country = util.get_str_value('Origem da fabricação da guitarra: ').capitalize().strip()
+    country = util.get_str_value('Origem da fabricação da guitarra: ')
     cost = util.get_float_value('Valor do produto: R$')
-    description = input('Descrição da guitarra: ').capitalize().strip()
+    description = input('Descrição da guitarra: ').title().strip()
     guitars[serial] = {
         CONST_FIELD_MODEL: model,
         CONST_FIELD_BRAND: brand,
         CONST_FIELD_YEAR: year,
         CONST_FIELD_COUNTRY: country,
         CONST_FIELD_COST: cost,
-        CONST_FIELD_DESCRIPTION: description
+        CONST_FIELD_DESCRIPTION: description,
+        CONST_FIELD_TOTAL: 0
     }
-    print('Guitarra cadastrada com sucesso!')
+
+
+def guitar_add(guitars: dict):
+    """
+    Registrar guitarra e suas especificações
+
+    :param guitars: Caracteristicas da guitarra ao cadastra-la
+    :return: Se a guitarra já está registrada ou não
+    """
+    serial = util.get_int_value('Informe o número de série da guitarra: ')
+    if serial in guitars:
+        choice = util.yes_or_no_value('Guitarra já cadastrada! Deseja alterar seus dados? [S/N]')
+        if choice == 'S':
+            guitar_register(serial, guitars)
+            return True, 'Dados da guitarra alterados com sucesso! '
+        else:
+            return False, 'Nenhum dado foi alterado ... '
+    else:
+        guitar_register(serial, guitars)
+        return True, 'Guitarra cadastrada com sucesso! '
 
 
 def guitar_edit(guitars: dict):
@@ -71,20 +96,19 @@ def guitar_edit(guitars: dict):
     Altera dados dos registros da guitarra
 
     :param guitars: Exibe os dados dos registros de guitarras
-    :return: None
+    :return: Se a guitarra sofreu alterações nos dados ou não
     """
-    serial = util.get_int_value('Número de série da guitarra: ')
+    serial = util.get_int_value('Informe o número de série da guitarra a ser editada: ')
     if serial in guitars:
-        guitars.pop(serial)
-        guitar_register(guitars)
-        return True, "Especificações da guitarra alteradas com sucesso! "
+        guitar_register(serial, guitars)
+        return True, 'Cliente alterado com sucesso! '
     else:
-        choice = util.yes_or_no_value('Guitarra não localizada. Deseja cadastrá-la?').strip().upper()[0]
-        if choice == "S":
-            guitar_register(guitars)
-            print('Guitarra cadastrada com sucesso! ')
+        choice = util.yes_or_no_value('Guitarra não localizado! Deseja cadastrá-la? [S/N]')
+        if choice == 'S':
+            guitar_register(serial, guitars)
+            return True, 'Guitarra cadastrada com sucesso! '
         else:
-            print('Guitarra não localizada! ')
+            return False, 'Guitarra não foi cadastrada... '
 
 
 def search_guitar(guitars: dict):
@@ -92,21 +116,20 @@ def search_guitar(guitars: dict):
     Procura guitarras no banco de dados
 
     :param guitars: Possibilita a pesquisa de guitarras registradas
-    :return: None
+    :return: Se a guitarra foi encontrada ou não
     """
-    serial = util.get_int_value('Número de série da guitarra: ')
+    serial = util.get_int_value('Informe o número de série da guitarra que deseja pesquisar: ')
     if serial in guitars:
-        print(f'Modelo da guitarra: {guitars[serial][CONST_FIELD_MODEL]}')
-        print(f'Marca da guitarra: {guitars[serial][CONST_FIELD_BRAND]}')
-        print(f'Ano de fabricação da guitarra: {guitars[serial][CONST_FIELD_YEAR]} ')
-        print(f'Origem de fabricação da guitarra: {guitars[serial][CONST_FIELD_COUNTRY]} ')
-        print(f'Custo da guitarra: {guitars[serial][CONST_FIELD_COST]} ')
+        print(f'Modelo da guitarra: {guitars[serial][CONST_FIELD_MODEL]} ')
+        print(f'Marca da guitarra: {guitars[serial][CONST_FIELD_BRAND]} ')
+        print(f'Ano de Fabricação da guitarra: {guitars[serial][CONST_FIELD_YEAR]} ')
+        print(f'País de Fabricação da guitarra: {guitars[serial][CONST_FIELD_COUNTRY]} ')
+        print(f'Custo da guitarra: R${guitars[serial][CONST_FIELD_COST]} ')
         print(f'Descrição da guitarra: {guitars[serial][CONST_FIELD_DESCRIPTION]} ')
-        util.sleep()
     else:
-        choice = util.yes_or_no_value('Guitarra não localizada. Deseja cadastrá-la? [S/N]').strip().upper()[0]
+        choice = util.yes_or_no_value('Guitarra não localizada. Deseja cadastrá-la? [S/N]')
         if choice == "S":
-            guitar_register(guitars)
+            guitar_register(serial, guitars)
             print('Guitarra cadastrada com sucesso! ')
         else:
             print('Guitarra não localizada! ')
@@ -119,51 +142,84 @@ def list_guitar(guitars: dict):
     :param guitars: Exibe as guitarras registradas no dicionário
     :return: None
     """
-    for k, v in guitars.items():
-        print(f'Número de série da guitarra e suas características -> {k} : {v}')
-    util.sleep()
+    if len(guitars) > 0:
+        for k, v in guitars.items():
+            print(f'Número de série da guitarra e suas características -> {k} : {v}')
+    else:
+        print('Antes de listar produtos, você precisa registra-lo... ')
+        serial = util.get_int_value('Informe o número de série da guitarra: ')
+        choice = util.yes_or_no_value('Deseja cadastrá-la? [S/N]')
+        if choice == "S":
+            guitar_register(serial, guitars)
+            print('Guitarra cadastrada com sucesso! ')
+        else:
+            print('Você optou por não registrar nenhum produto no momento. ')
 
 
-def stockpile_remove(guitars: dict):
+def stockpile_modify(guitars: dict):
     """
-    Remoção de guitarras do estoque
+    Adicionar ou remover guitarras do estoque
 
     :param guitars: Identifica guitarras no estoque a serem removidos
     :return: None
     """
     serial = util.get_int_value('Digite o número de série da guitarra: ')
     if serial in guitars:
-        choice = util.yes_or_no_value('Certeza que deseja remover a guitarra do estoque [S/N]: ').strip().upper()[0]
-        if choice == 'S':
-            del guitars[serial]
-            print('Guitarra removida do estoque com sucesso! ')
+        choice = util.get_int_value_with_range('[1] Adicionar itens ao estoque\n'
+                                               '[2] Remover itens do estoque\n'
+                                               'Escolha: ', 1, 2)
+        if choice == 1:
+            add = util.get_int_value('Quantidade de itens para adicionar: ')
+            guitars[serial][CONST_FIELD_TOTAL] += add
+            print(f'{add} itens foram adicionados com sucesso! ')
         else:
-            print('Nenhuma guitarra foi removida do estoque! ')
+            subtract = util.get_int_value('Quantidade de itens para remover: ')
+            if guitars[serial][CONST_FIELD_TOTAL] - subtract < 0:
+                print(f'A quantia de itens no estoque não pode ser inferior a zero...')
+                choice = util.yes_or_no_value('Gostaria de ver quantos itens você tem no momento? [S/N]')
+                if choice == 'S':
+                    print(f'Existem {guitars[serial][CONST_FIELD_TOTAL]} itens no estoque.')
+                else:
+                    print('Você optou por não visualizar o total de itens no estoque...')
+            guitars[serial][CONST_FIELD_TOTAL] -= subtract
+            print('Itens foram removidos com sucesso! ')
+    else:
+        choice = util.yes_or_no_value('Guitarra não localizada no estoque. Deseja adiciona-la? [S/N]')
+        if choice == "S":
+            guitar_register(serial, guitars)
+            print('Guitarra adicionada ao estoque com sucesso! ')
+        else:
+            print('Nenhuma guitarra foi reposta no estoque... ')
 
 
-def main():
-    guitars = {}
+def main(guitars: dict):
+    """
+    Ponto principal do programa
+    """
     while True:
         choice = menu()
         if choice == 1:
-            guitar_register(guitars)
+            res, resp = guitar_add(guitars)
+            print(resp)
         if choice == 2:
-            guitar_edit(guitars)
+            res, resp = guitar_edit(guitars)
+            print(res)
         if choice == 3:
             search_guitar(guitars)
         if choice == 4:
             list_guitar(guitars)
         if choice == 5:
-            stockpile_remove(guitars)
+            stockpile_modify(guitars)
         if choice == 6:
-            leave = util.yes_or_no_value('Certeza de que deseja sair do programa? [S/N]').strip().upper()[0]
+            leave = util.yes_or_no_value('Certeza de que deseja sair do programa? [S/N]')
             if leave == 'S':
                 print('Volte sempre! ')
+                util.save_in_file_dict("guitarras", guitars)
                 break
             else:
-                print('Você optou por não sair do programa! ')
-                util.sleep()
+                print('Você optou por não sair do programa... ')
 
 
 if __name__ == '__main__':
-    main()
+    var = util.file_read_bin("guitarras")
+    main(var)
