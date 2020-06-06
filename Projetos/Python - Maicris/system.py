1ddc3b601041f31e4afb92c7477c6e76db28532f
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-06-06 11:21:37 -0300

    System

--- Projetos/Python - Maicris/system.py
+++ Projetos/Python - Maicris/system.py
@@ -1,19 +1,42 @@
 """
 Sales System
+
+sales = {
+    id_venda: {                         sales = { id_venda: {id_client: "bla", products: [ {id_product: "asd", quantidade: ?}, {id_product: "asd", quantidade: ?} ], total: ? } }
+        id_client: "",
+        guitars: [
+            {
+                id_product: "",
+                quant: 0
+            },
+            {
+                id_product: "".
+                quant: 1
+            }
+        ],
+        total: 100
+    }
+}
+
 """
 
 
 import utilities as util
-import product as prod
-import client as cli
+import guitars as gui
+import clients as cli
 import pickle
 
 
+CONST_FIELD_QUANTITY = "Quantidade"
+CONST_FIELD_VALUE = "Valor Total"
+CONST_FILE_NAME = "database"
+
+
 def menu():
-    util.ornament('Sistema de Vendas')
-    util.ornament_two('-----SUAS OPÇÕES-----')
+    print('Sistema de Vendas')
+    print('-----SUAS OPÇÕES-----')
     print('[1] Criar Pedido\n'
-          '[2] Cancelar Pedidos\n'
+          '[2] Cancelar Pedido\n'
           '[3] Resumo de Pedidos\n'
           '[4] Relatório de Pedidos Gerais\n'
           '[5] Relatório de Pedidos por Cliente\n'
@@ -22,7 +45,32 @@ def menu():
     return util.get_int_value_with_range('Digite uma das opções: ', 1, 6)
 
 
-def create_order():
+def create_order(clients: dict, guitars: dict, sales: dict, identification: int, serial: int):
+    util.file_read_bin("cliente")
+    resp = cli.client_search(clients)
+    if resp:
+        print('Venda será prosseguida! ')
+        ans = gui.search_guitar(guitars)
+        if ans:
+            print('Guitarra disponível! ')
+
+
+        else:
+            print('Guitarra não encontrada... ')
+            choice = util.yes_or_no_value('Guitarra não localizado! Deseja cadastrá-la? [S/N]')
+            if choice == 'S':
+                gui.guitar_register(serial, guitars)
+                return True, 'Guitarra cadastrada com sucesso! '
+            else:
+                return False, 'Guitarra não foi cadastrada... '
+    else:
+        print('Cliente não cadastrado no sistema... ')
+        choice = util.yes_or_no_value('Cliente não localizado. Deseja cadastra-lo? [S/N]')
+        if choice == "S":
+            cli.client_register(identification, clients)
+            print('Cliente cadastrado com sucesso! ')
+        else:
+            print('Cliente não foi cadastrado... ')
 
 
 def cancel_order():
@@ -38,27 +86,48 @@ def specific_order_report():
 
 
 def main():
+    # database = {
+        #"Cliente": clients,
+        #"Guitarras": guitars,
+        #"Venda": sales
+    # }
+
     sales = {}
+    guitars = {}
+    clients = {}
+
+    database = util.file_read_bin(CONST_FILE_NAME)
+    if "clients" in database:
+        clients = database["Clients"]
+    if "products" in database:
+        guitars = database["Products"]
+    if "sales" in database:
+        sales = database["sales"]
     while True:
         choice = menu()
         if choice == 1:
-            create_order()
-        if choice == 2:
+            create_order(clients, guitars, sales)
+        elif choice == 2:
             cancel_order()
-        if choice == 3:
+        elif choice == 3:
+            cli.main(clients)  #Maicris fez
             order_summary()
-        if choice == 4:
+        elif choice == 4:
             general_order_report()
-        if choice == 5:
+        elif choice == 5:
             specific_order_report()
-        if choice == 6:
-            leave = util.yes_or_no_value('Certeza de que deseja sair do programa? [S/N]').strip().upper()[0]
+        elif choice == 6:
+            leave = util.yes_or_no_value('Certeza de que deseja sair do programa? [S/N]')
             if leave == 'S':
                 print('Volte sempre! ')
                 break
             else:
                 print('Você optou por não sair do programa! ')
-                util.sleep()
+
+    database["clients"] = clients
+    database["guitars"] = guitars
+    database["sales"] = sales
+    util.file_write_bin(CONST_FILE_NAME, database)
 
 
 if __name__ == '__main__':
